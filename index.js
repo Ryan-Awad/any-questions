ab5089e4fb3ac66238df05420d435f097f253122
Ryan Awad <ryanawad@yahoo.com>
2022-06-11 22:14:46 -0400

    Started to add JWT authentication

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
+require('dotenv').config();
 const express = require('express');
+var {expressjwt: jwt} = require("express-jwt");
 const {writeData, readData} = require('./src/firebaseOperations');
 const app = express();
+
+
 app.use(express.json());
+
+app.use(jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ['HS256']
+  }).unless({path: ['/ping']})
+);
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token.\n');
+  } else {
+    next(err);
+  }
+});
 
 app.get('/ping', (req, res) => {
   res.send('Pong!');
 });
 
-// ** ADD AUTHENTICATION **
-app.post('/upload-question', (req, res) => {
+app.post('/upload-question', (req, res) => { // IF INVALID BODY IS SENT, THE BACKEND THROWS ERROR AND CRASHES (FIX) 
   const {title, body, imageURL} = req.body;
   const data = {
     title: title,
     body: body,
-    imgURL: imageURL
-  }
+    imgURL: imageURL,
+    answered: false
+  };
 
   writeData('questions', data, () => {
     res.send('Question successfully uploaded!\n');
