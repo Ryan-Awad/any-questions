d955ba044a0d8ff91a3e8386a0fd3755ae1d3d95
Ryan Awad <ryanawad@yahoo.com>
2022-06-11 21:29:18 -0400

    added more functionality to the backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {writeData, readData} = require('./src/firebaseOperations');
 const app = express();
 app.use(express.json());
 
@@ -6,11 +7,24 @@ app.get('/ping', (req, res) => {
   res.send('Pong!');
 });
 
+// ** ADD AUTHENTICATION **
 app.post('/upload-question', (req, res) => {
   const {title, body, imageURL} = req.body;
-  
+  const data = {
+    title: title,
+    body: body,
+    imgURL: imageURL
+  }
 
-  res.send('Question uploaded successfully.\n');
+  writeData('questions', data, () => {
+    res.send('Question successfully uploaded!\n');
+  });
+});
+
+app.post('/get-questions', (req, res) => {
+  readData('questions', data => {
+    res.send(data);
+  });
 });
 
 app.listen(8080, () => {
--- src/firebaseOperations.js
+++ src/firebaseOperations.js
@@ -2,27 +2,35 @@ const firebase = require('firebase-admin');
 const serviceAccount = require('../auth/serviceAccount.json');
 
 firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount)
+  credential: firebase.credential.cert(serviceAccount)
 });
 
 const db = firebase.firestore();
 
-getID = () => {
-    const idLength = 20;
-    const id = Math.round(Math.random() * 10**idLength);
-    // check if ID exists
-    return id.toString();
+module.exports.getID = (collection, callback) => {
+  const idLength = 20;
+  const id = Math.round(Math.random() * 10**idLength);
+
+  this.readData(collection, data => {
+    const ids = data.map(x => x.id);
+    if (ids.includes(id)) {
+      getID(collection, callback); // recurse to try generating another ID
+    } else {
+      callback(id.toString());
+    }
+  });
 }
 
-writeData = (collection, data, callback) => {
-    const dataID = getID();
+module.exports.writeData = (collection, data, callback) => {
+  this.getID(collection, dataID => {
+    data['id'] = dataID;
     db.collection(collection).doc(dataID).create(data);
     callback();
+  });
 }
 
-readData = (collection, callback) => {
-    db.collection(collection).get().then(data => {
-        callback(data.docs.map(doc => doc.data()));
-    });
+module.exports.readData = (collection, callback) => {
+  db.collection(collection).get().then(data => {
+    callback(data.docs.map(doc => doc.data()));
+  });
 }
-
