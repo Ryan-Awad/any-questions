1ffec0e159183726bd42b52d509d465e44de327d
Ryan Awad <ryanawad@yahoo.com>
2022-06-18 18:47:49 -0400

    finished login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,37 +2,51 @@ import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 class Login extends Component {
-  handleSubmit = (event) => {
-    const username = event.target.username.value;
-    const password = event.target.password.value;
+  state = {
+    typedUsername: null,
+    typedPassword: null
+  }
+
+  handleUserChange = (e) => this.state.typedUsername = e.target.value;
+  handlePassChange = (e) => this.state.typedPassword = e.target.value;
+
+  signIn = () => {
+    const {typedUsername, typedPassword} = this.state;
     fetch('https://any-questions-backend.herokuapp.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        username: username,
-        password: password
+        username: typedUsername,
+        password: typedPassword
       })
     })
-    .then(res => res.json()) // *** edit the backend so that it returns JSON ***
-    .then(data => {
-      //console.log(data);
-      alert(data);
-    })
+    .then(res => {
+      if (res.status === 401) {
+        alert('Invalid credentials.');
+      } 
+      else if (res.status === 200) {
+        res.json().then(data => {
+          const jwt = data.success;
+          document.cookie = `token=${jwt}`; // add the jwt to the user's cookies
+          window.location.href = '/'; // redirect user to home page
+        })
+      }
+    });
   }
 
   render() { 
     return (
-      <Form onSubmit={this.handleSubmit}>
+      <Form>
         <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control as="input" name="username"/><br></br>
+          <Form.Control as="input" name="username" onChange={this.handleUserChange}/><br></br>
 
           <Form.Label>Password</Form.Label>
-          <Form.Control as="input" type="password" name="password"/><br></br>
+          <Form.Control as="input" type="password" name="password" onChange={this.handlePassChange}/><br></br>
 
-          <Button type="submit">Sing In →</Button>
+          <Button type="button" onClick={this.signIn}>Sing In →</Button>
         </Form.Group>
       </Form>
     );
